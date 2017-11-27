886d260995d6ef248576eb6fc724c68f361e8a04
sarphiv <32750711+sarphiv@users.noreply.github.com>
2017-11-27 11:03:22 +0100

    Fixed LapCounter, used EQU instead of RES, bricked EEPROM.

--- main.asm
+++ main.asm
@@ -14,7 +14,25 @@ INCLUDE "p16f684.inc"
 
     ORG	    0x0004	;Interrupt service routine vector
     goto    ISR
-	
+
+ ; ****** Memory and constants ***********************************
+ ;RSTAT bit definitions
+    RUN		EQU	0   ;Running - The state of motor. 1 = Race, 0 = Stop
+    MODE	EQU	1   ;Mode - Toggle reading/write to EEPROM. 1 = Recon, 0 = Race
+    BANKBIT	EQU	0   ;Status bank bit
+    TOTALLAPS   EQU	4   ;Total number of laps minus 1
+			    ;Because start is included as a lap
+    
+    cblock  0x20
+    RSTAT		    ;Status register for car
+    ISR_WCopy		    ;Variable for temporarily storing working register
+    ISR_BCopy		    ;Variable for temporarily storing bits
+    EE_DATA
+    EE_ADR
+    EE_MASK
+    LAP_Laps
+    endc
+
 ; ******* MODULES *******************************************************
 #INCLUDE    "PWM.inc"
 #INCLUDE    "HallEffect.inc"
@@ -25,13 +43,10 @@ INCLUDE "p16f684.inc"
 	
 ; ******* SETUP *****************************************************
 INIT
-    TEST    RES	1	;currently testing things
-    
-    RSTAT   RES	1	    ;Status register for car
-   ;RSTAT bit definitions
-    RUN	    EQU	0	    ;Running - The state of the race. 1 = Race, 0 = Stop
-    MODE    EQU	1	    ;Mode - Toggle reading/write to EEPROM. 1 = Recon, 0 = Race
-    
+    ;Zero RSTAT address
+    BCF	    STATUS, RP0	    ;Change to bank 0
+    MOVLW   h'00'
+    MOVWF   RSTAT
     
     ;Switch pin setup (this is only temporarily left here)
     BSF	    STATUS, RP0	    ;Change to bank 1
@@ -45,17 +60,21 @@ INIT
     MOVLW   b'11110111'	    ;Set C3 (UNUSED) to output
     ANDWF   TRISC, F
     
+    
     CALL    PWM_INIT
     CALL    HallEffect_INIT
     
     CALL    LDR_INIT
     CALL    LAP_INIT
     
-    CALL    EE_INIT
+;    CALL    EE_INIT
 
     CALL    ISR_INIT
     
     
+    BCF	    STATUS, RP0
+;    BSF	    PORTC, 3
+    BSF	    RSTAT, MODE	    ;Recon mode
     ;C5 (PWM signal), C4 (Comparator out), C3 UNUSED, C2 (Turn switch), C1 C0 (Comparator -, +)
     ;A2 (Hall effect interrupt) input, A3 (State switch) (These are both input by default)
     
@@ -63,22 +82,23 @@ INIT
 
 ; ******* MAIN LOOP *****************************************************	
 LOOP
-;    BTFSS   TEST, 0
-;    BSF	    PORTC, 3
-;    BTFSC   TEST, 0
-;    BCF	    PORTC, 3
-;    Test program
-    BCF	    STATUS, RP0
+    BCF	    STATUS, RP0		;Change to bank 0
+    BTFSC   RSTAT, RUN
+    BSF	    PORTC, 3
+    BTFSS   RSTAT, RUN
+    BCF	    PORTC, 3
     
-    BTFSS   PORTC, 2
-    MOVLW   d'150'
-    BTFSC   PORTC, 2
-    MOVLW   d'140'
-    
-    CALL    PWM_SET
-;    NOP
+;   Turn switch control program
+;    BCF	    STATUS, RP0
+;    
+;    BTFSS   PORTC, 2
+;    MOVLW   d'150'
+;    BTFSC   PORTC, 2
+;    MOVLW   d'140'
+;    
+;    CALL    PWM_SET
+    NOP
 
-LOOPEND
     GOTO    LOOP
 
     END
