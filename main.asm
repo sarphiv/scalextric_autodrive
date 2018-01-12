2ff347a599d9087fe76798e4650190985565112d
sarphiv <32750711+sarphiv@users.noreply.github.com>
2018-01-12 09:54:17 +0100

    Tunings and forgotten fixes

--- main.asm
+++ main.asm
@@ -25,12 +25,12 @@ INCLUDE "p16f684.inc"
     ;LDR constants
     TOTALLAPS		EQU	3	;Total number of laps
     ;Motor constants
-    SPEED_RACE_TURN	EQU	d'80'	;Speed while turning in race mode
-    SPEED_RACE_STRAIGHT	EQU	d'145'	;Speed driving straight in race mode
+    SPEED_RACE_TURN	EQU	d'75'	;Speed while turning in race mode
+    SPEED_RACE_STRAIGHT	EQU	d'125'	;Speed driving straight in race mode
     SPEED_RECON_NORMAL	EQU	d'85'	;Speed in recon mode
-    SPEED_RACE_ACCEL	EQU	d'190'	;Speed while accelerating in race mode
-    ACCEL_RACE_LENGTH	EQU	d'5'	;Cycles to accelerate in (8 is around 25 cm)
-    EE_LOOK		EQU	d'1'	;Look-ahead distance in bytes
+    SPEED_RACE_ACCEL	EQU	d'220'	;Speed while accelerating in race mode
+    ACCEL_RACE_LENGTH	EQU	d'6'	;Cycles to accelerate in (8 is around 25 cm)
+    EE_LOOK		EQU	d'2'	;Look-ahead distance in bytes
 
     
     cblock  0x20
